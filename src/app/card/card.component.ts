5f5db18a0cf1dfe3b491da78ad9998cbc399aba8
NataliiaKoval <hakunata@gmail.com>
2021-03-02 12:26:22 +0200

    sending properties for card from app

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() username = '';
+  @Input() content = '';
 
 }
